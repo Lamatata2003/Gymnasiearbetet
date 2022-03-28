8194cf2c9e0b465ea5739337254335185ffb096f
hauii <hampushauii@gmail.com>
2022-03-28 20:45:11 +0200

    a

--- learn.php
+++ learn.php
@@ -3,9 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css.css">
 	<title></title>
 </head>
 <body class="bg-color">
-YEP
+
 </body>
 </html>
