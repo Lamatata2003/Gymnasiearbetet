2a6086db6bf15d9d243f68e7a72db7f489a8402c
hauii <hampushauii@gmail.com>
2022-03-29 20:36:28 +0200

    updated bootstrap links

--- questionhtml.php
+++ questionhtml.php
@@ -1,5 +1,5 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="javascript.js"></script>
 <script type="text/javascript" src="jquery-3.6.0.js"></script>
 
