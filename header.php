2fb6ebf3fb243896625410939c7060ca4587cbea
Lamatata2003 <liam.mineur@outlook.com>
2022-03-30 19:32:54 +0200

    added some titles

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark navcolor">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Home</a>
+    <a class="navbar-brand" href="index.php">Gengobu</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
