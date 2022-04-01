f167e4ca5c9f12bd96712cfd872d125e2ba0f502
Hampus Grudemo <hampus.grudemo@learnet.se>
2022-04-01 08:55:56 +0200

    a

--- header.php
+++ header.php
@@ -12,13 +12,12 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="align-content-center">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <a class="nav-link" href="#">Learning</a>
       </li>
-
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
@@ -30,6 +29,8 @@
       <li class="nav-item">
         <a class="nav-link" href="#">About us</a>
       </li>
+      
+      
 
 
 <div class="dropdown">
