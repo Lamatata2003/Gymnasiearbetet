30ce3175f763a5d035e1de5d129c2547dfee532a
Hampus Grudemo <hampus.grudemo@learnet.se>
2022-04-05 10:55:18 +0200

    fixed dropdown menu on kanachart page

--- kanachart.php
+++ kanachart.php
@@ -2,7 +2,6 @@
 <html>
 <head>
 	<script type="text/javascript" src="jquery-3.6.0.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css.css">
 	<meta charset="utf-8">
