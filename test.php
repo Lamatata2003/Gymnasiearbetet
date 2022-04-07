4bc11cf02c665716ef1fb6ca3c557ee3cbd8e8a3
hauii <hampushauii@gmail.com>
2022-04-07 21:57:26 +0200

    redesigned test page

--- test.php
+++ test.php
@@ -1,45 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <script src="javascript.js"></script>
+    <script type="text/javascript" src="jquery-3.6.0.js"></script>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css.css">
-	<title>Quiz - Gengobu</title>
+	<title></title>
 </head>
+<?php include("header.php")?>
 <body class="bg-color">
-	<div>
-
-		<div class="d-flex flex-column align-content-center flex-wrap">
-		<?php // Takes raw question data from the file
-
-		$var = "";
-		$arr = ["hiragana_questions.json","katakana_questions1.json"];
-		if (isset($_POST["json"]) == "hiragana_questions") {
-			$var = $arr[0];
-		}
-		if (isset($_POST["json"])) {
-		
-			$json = file_get_contents($var);
-			// Converts it into a PHP object
-			$data = json_decode($json);
-			shuffle($data);
-			$qs = [];
-			foreach ($data as $value) {
-				$qs[] = new question($value[0],$value[1]);
-			}
-			$_SESSION["data"]=serialize($qs);
-			$_SESSION["answers"]=[];
-		}
-		?>
-		<div class="">
-			<form action="" method="post">
-			<!--<input type="text" name="name" value="" placeholder="名前">-->
-			 <input type="hidden" name="question" value="0">
-		 	 <input type="hidden" name="answers" value="">
-<!-- 			<button type="submit" class="btn btn-secondary">Start quiz</button> -->
-</form>
-			<div class="d-flex justify-content-center">
+	<div class="d-flex justify-content-center">
 	<h1 class="greycolor margintop">Test your knowledge</h1>
 	</div>
 	<div class="d-flex flex-row justify-content-center margintop">
@@ -114,6 +86,8 @@
 	<div class="flexbox-item"></div>
 </div>
 
+</body>
+</html>
 <style type="text/css">
 	.flexbox-item {
 		width: 30px;
@@ -179,6 +153,3 @@
   	margin-bottom: 32px;
   }
 </style>
-</body>
-</html>
-
